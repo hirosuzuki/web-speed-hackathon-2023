ff397eb86c63cf1e99541c37e7b1bd2e409b5f04
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-03-04 23:33:59 +0900

    cache 120sec

--- src/server/index.ts
+++ src/server/index.ts
@@ -48,7 +48,8 @@ async function init(): Promise<void> {
   app.use(session({}, app));
 
   app.use(async (ctx, next) => {
-    ctx.set('Cache-Control', 'no-store');
+    // ctx.set('Cache-Control', 'no-store');
+    ctx.set('Cache-Control', 'max-age=120');
     await next();
   });
 
