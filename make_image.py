36048152bc24427586f0bec453eec29d102f9fa5
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-03-04 18:54:56 +0900

    画像変換Buxgix (not orig)

--- make_image.py
+++ make_image.py
@@ -2,10 +2,10 @@ import subprocess
 
 files = subprocess.check_output(["find", "orig-public", "-name", "*.jpg"]).decode("utf8").split("\n")
 for fn in files:
-    orig_fn = fn[5:].strip()
+    orig_fn = fn.strip()
     if orig_fn:
         print("*", orig_fn)
-        new_fn = orig_fn[:-4] + ".jpg"
+        new_fn = orig_fn[5:-4] + ".jpg"
         subprocess.call(["convert", orig_fn, "-quality", "85", new_fn])
         new_fn = orig_fn[:-4] + "-s.jpg"
         subprocess.call(["convert", orig_fn, "-resize", "224", "-quality", "85", new_fn])
